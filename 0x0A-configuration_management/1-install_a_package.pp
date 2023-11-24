4206bf6dc45fbbab9a04329805d9a146719b3dd1
Lawal Tajudeen Ogunsola <lwltjdn@gmail.com>
2023-11-25 00:11:12 +0100

    sing Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # Using the Puppet to install flask from pip3
 package { 'flask':
-  ensure   => '2.1.0',
+  # ensure   => '2.1.0',
+  ensure   => Installed,
   provider => 'pip3',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0'
+  path    => ['/usr/bin'],
+  require => Package[python3-pip],
 }
