d2b491a36c9e1bc320b6a6633dba6ecb6f62320f
LawalTheWest <lwltjdn@gmail.com>
2024-03-24 12:52:52 +0100

    Using Puppet, install flask from pip3.
    Requirements:
    
    Install flask
    Version must be 2.1.0

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,23 @@
+# Installing the flask from pip3 using puppet
+
+# Requirements:
+# Install flask
+# Version must be 2.1.0
+
+
+# We have ensure python3 and pip3 are installed
+package { 'python3':
+  ensure => 'installed',
+}
+
+# pip3 istallation confirmation
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+# We need to install Flask using the pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => [Package['python3'], Package['python3-pip']],
+}
