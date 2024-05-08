8d86ac4bde4903b5fe0229a6b5422d67f7f3283b
lawalTheWest <lwltjdn@gmail.com>
2024-05-08 14:40:35 +0100

    updated to adjust syntax

--- 0x16-api_advanced/2-main.py
+++ 0x16-api_advanced/2-main.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == '__main__':
     recurse = __import__('2-recurse').recurse
     if len(sys.argv) < 2:
-        usage = 'Please pass an argument for the subreddit to search.')
+        usage = 'Please pass an argument for the subreddit to search.'
         print(usage)
     else:
         result = recurse(sys.argv[1])
