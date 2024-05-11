49fc04d38a615b67aa92551224d6e1a10d1c809c
lawalTheWest <lwltjdn@gmail.com>
2024-05-11 16:00:27 +0100

    fixing the typo of an APache running the Error 500

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# The Apache returns 500; using this script to fix typo in configuration
+
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
