ac3674c5b8329547356c3dcbf7d0cbbb8b78214e
lawalTheWest <lwltjdn@gmail.com>
2024-05-13 16:10:28 +0100

    changing os config

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# Change the OS configuration so that it is possible to login
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
